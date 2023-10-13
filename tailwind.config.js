8f5a705b6e9bbb58d2c94e234db745d4305e52e4
danielpuliche <danielpuliche@unicauca.edu.co>
2023-10-12 19:10:00 -0500

    Estilos base y edicion tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html','./src/**/*.{vue,jsx,js,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'strongPurpleTwitch': 'rgb(100,65,165)',
+        'lightPurpleTwitch': 'rgb(185,163,227)',
+        'BlackTwitch': 'rgb(38,38,38)',
+        'whiteTwitch': 'rgb(241,241,241)',
+      },
+    },
   },
   plugins: [],
 }
